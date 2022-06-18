12743a97e9ecac7bdf932ed5449cd0e03941f746
sofiafer589 <88357614+sofiafer589@users.noreply.github.com>
2022-06-18 19:21:53 -0300

    a

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -30,22 +30,26 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += movement * Time.deltaTime * Speed;
         
-        if(Input.GetAxis("Horizontal") > 0f)
+        float movement = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if(movement > 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f,0f,0f);
         }
         
-        if(Input.GetAxis("Horizontal") < 0f)
+        if(movement < 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f,180f,0f);
         }
 
-        if(Input.GetAxis("Horizontal") == 0f)
+        if(movement == 0f)
         {
             anim.SetBool("walk", false);
         }
