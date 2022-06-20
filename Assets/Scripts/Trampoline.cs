d77937e8461615b546309e31f75e108d4377df95
sofiafer589 <88357614+sofiafer589@users.noreply.github.com>
2022-06-20 18:25:20 -0300

    trampoline

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public float jumpForce;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("jump");
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
         
-    }
+    }   
 }
